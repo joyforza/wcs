455ebe1f3909683fa2569ce259c64aeff6dc87ef
sonlh <lehungson007@gmail.com>
2018-10-04 11:44:41 +0700

    Update & fix bug again :'(

--- wcssitemap/src/main/java/Main.java
+++ wcssitemap/src/main/java/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         String url = "https://block.vn";
+        String url2 = "https://thethao.vnexpress.net";
+
         SiteMap siteMap = new SiteMap(url);
         siteMap.createMap();
     }
