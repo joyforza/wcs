633a321fb884cd6b0f3f952ba55f3405acca945f
sonlh <lehungson007@gmail.com>
2018-10-09 09:17:46 +0700

    Add mapping graph to String to generate 'Site-map-vision'

--- wcssitemap/src/main/java/Main.java
+++ wcssitemap/src/main/java/Main.java
@@ -1,13 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         String url = "https://block.vn";
+        String url2 = "https://tuyendung.tiki.vn";
 
 
-        SiteMap siteMap = new SiteMap(url);
+        SiteMap siteMap = new SiteMap(url2);
         siteMap.createMap();
 
-        //siteMap.getLinks("https://vnexpress.net");
-        siteMap.makeInverseGraph();
-        siteMap.printReferenceLinks("https://block.vn/dang-nhap");
+        //siteMap.getLinks(url);
+
+        // comments:
+        //siteMap.makeInverseGraph();
+        //siteMap.printReferenceLinks("https://block.vn/dang-nhap");
+
+        System.out.println(siteMap.getDecodeGraph());
     }
 }
